6bcfa6a4923a56872478b70d16867ffdb068b0a2
João Vitor Matos <jvvm4t0s@gmail.com>
2024-05-23 16:52:32 -0300

    Correção ao excluir os filmes

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -2,21 +2,30 @@ const { test, expect } = require('../support')
 
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
-
-test.beforeAll(async () => {
-    await executeSQL(`DELETE from movies`)
-})
+const { request } = require('http')
 
 test('Deve cadastrar um novo filme', async ({ page }) => {
     const movie = data.create
+    await executeSQL(`DELETE from movies WHERE name = "${movie.title}"`)
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.create(movie)
     await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`)
 })
 
+test('Deve excluir um filme', async ({ page, request })=>{
+    const movie = data.to_remove
+    await executeSQL(`DELETE from movies WHERE name = "${movie.title}"`)
+    await request.api.postMovie(movie)
+
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.remove(movie.title)
+    await page.popup.haveText('Filme removido com sucesso.')
+})
+
 test('Não deve cadastrar filmes com títulos duplicados', async ({ page, request }) => {
     const movie = data.duplicate
+    await executeSQL(`DELETE from movies WHERE name = "${movie.title}"`)
     await request.api.postMovie(movie)
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
